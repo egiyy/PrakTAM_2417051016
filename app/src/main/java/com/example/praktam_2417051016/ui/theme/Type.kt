68e8d6bfee69d7477e81f3ff672b5b0e009d3fe3
Reggy Desvita <reggydesvita5@gmail.com>
2026-04-02 14:16:54 +0700

    Tugas LKP 6

--- app/src/main/java/com/example/praktam_2417051016/ui/theme/Type.kt
+++ app/src/main/java/com/example/praktam_2417051016/ui/theme/Type.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+
 val AppTypography = Typography(
 
     titleLarge = TextStyle(
